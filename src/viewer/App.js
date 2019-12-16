fde8b19ceafa0ef31abb15c213b3e81898e9db2a
k.baldin <k.baldin@softpro.com>
2019-12-16 20:33:32 +0300

    add name regexp refactor color undated font awesome

--- src/viewer/App.js
+++ src/viewer/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Panel from 'react-flex-panel';
 import FontAwesome from 'react-fontawesome';
-import classNames from 'classnames';
+import cx from 'classnames';
 import HexViewer from './HexViewer';
 import { ObjectInspector } from 'react-inspector';
 
@@ -17,68 +17,147 @@ const stringToBuffer = str => {
   return ui8;
 };
 
-const TimeStamp = ({time}) => {
+const TimeStamp = ({ time }) => {
   const h = time.getHours();
   const m = time.getMinutes();
   const s = time.getSeconds();
   const ms = time.getMilliseconds();
-  return <span className="timestamp">{padded(h, 2)}:{padded(m, 2)}:{padded(s, 2)}.{padded(ms, 3)}</span>;
+  return (
+    <span className="timestamp">
+      {padded(h, 2)}:{padded(m, 2)}:{padded(s, 2)}.{padded(ms, 3)}
+    </span>
+  );
 };
 
-const FrameEntry = ({frame, selected, ...props}) => (
-  <li className={classNames("frame", "frame-" + frame.type, {"frame-selected": selected})} {...props}>
-    <FontAwesome name={frame.type === "incoming" ? "arrow-circle-down" : "arrow-circle-up"}/>
-    <TimeStamp time={frame.time}/>
-    <span className="name">{frame.text != null ? "Text Frame" : "Binary Frame"}</span>
-    <span className="length">{frame.length}</span>
-  </li>
-);
+class ListControls extends React.Component {
+  state = {
+    openInput: null // 'filter' | 'name'
+  };
 
-class FrameList extends React.Component {
+  openNameReg = () => {
+    if (this.state.openInput === 'name') {
+      this.setState({ openInput: null });
+    } else {
+      this.setState({ openInput: 'name' });
+    }
+  };
+
   render() {
-    const { frames, activeId, onSelect, onClear, onStart, onStop, ...props } = this.props;
+    const { onClear, onStart, onStop, regName, onRegName, capturing } = this.props;
     return (
-      <Panel {...props} className="LeftPanel">
-        <div className="list-controls">
-          <FontAwesome className="list-button" name="ban" onClick={onClear} title="Clear"/>
-          <FontAwesome className="list-button" name="play" onClick={onStart} title="Start"/>
-          <FontAwesome className="list-button" name="stop" onClick={onStop} title="Stop"/>
+      <div className="list-controls">
+        {capturing ? (
+          <span className="list-button record active" onClick={onStop} title="Stop" />
+        ) : (
+          <span className="list-button record" onClick={onStart} title="Start" />
+        )}
+        <FontAwesome className="list-button" name="ban" onClick={onClear} title="Clear" />
+        <span className={'separator'} />
+        <FontAwesome
+          className={cx('list-button', {
+            active: !!regName
+          })}
+          name="file-signature"
+          onClick={this.openNameReg}
+          title="Clear"
+        />
+        <div
+          className={cx('input-wrap', {
+            hide: this.state.openInput !== 'name'
+          })}
+        >
+          <input
+            className={'input'}
+            name={'reg-name'}
+            placeholder={'Name regexp:  type=".+"'}
+            value={regName}
+            onChange={onRegName}
+          />
         </div>
-        <ul className="frame-list" onClick={() => onSelect(null)}>
-          {frames.map(frame => <FrameEntry key={frame.id} frame={frame} selected={frame.id === activeId}
-            onClick={e => { onSelect(frame.id); e.stopPropagation(); }}/>)}
-        </ul>
-      </Panel>
+      </div>
     );
   }
 }
 
-const TextViewer = ({data}) => (
-  <div className="TextViewer tab-pane">
-    {data}
-  </div>
-);
+class FrameList extends React.Component {
+  render() {
+    const { frames, activeId, onSelect, regName } = this.props;
+    return (
+      <ul className="frame-list" onClick={() => onSelect(null)}>
+        {frames.map(frame => (
+          <FrameEntry key={frame.id} frame={frame} selected={frame.id === activeId} regName={regName} onSelect={onSelect} />
+        ))}
+      </ul>
+    );
+  }
+}
 
-const JsonViewer = ({data}) => (
+class FrameEntry extends React.Component {
+  handlerSelect = () => {
+    this.props.onSelect(this.props.frame.id);
+  };
+
+  getName() {
+    const { frame, regName } = this.props;
+    if (frame.text != null) {
+      debugger;
+      if (regName) {
+        try {
+          let matchAll = frame.text.matchAll(regexp);
+
+          debugger;
+          matchAll = Array.from(matchAll); // теп
+          const firstMach = matchAll[0][1] || matchAll[0][0];
+          if (firstMach) {
+            return firstMach;
+          }
+        } catch (e) {}
+      }
+      return 'Text Frame';
+    } else {
+      return 'Binary Frame';
+    }
+  }
+
+  render() {
+    let { frame, selected } = this.props;
+
+    return (
+      <li className={cx('frame', 'frame-' + frame.type, { 'frame-selected': selected })} onClick={this.handlerSelect}>
+        <FontAwesome name={frame.type === 'incoming' ? 'arrow-circle-down' : 'arrow-circle-up'} />
+        <TimeStamp time={frame.time} />
+        <span className="name">{this.getName()}</span>
+        <span className="length">{frame.length}</span>
+      </li>
+    );
+  }
+}
+
+const TextViewer = ({ data }) => <div className="TextViewer tab-pane">{data}</div>;
+
+const JsonViewer = ({ data }) => (
   <div className="JsonViewer tab-pane">
-    <ObjectInspector data={data}/>
+    <ObjectInspector data={data} />
   </div>
 );
 
 class FrameView extends React.Component {
-  state = {panel: null};
+  state = { panel: null };
 
   makePanel(name, title) {
-    return <li className={classNames("tab-button", {"active": this.state.panel === name})}
-      onClick={() => this.setState({panel: name})}>{title}</li>;
+    return (
+      <li className={cx('tab-button', { active: this.state.panel === name })} onClick={() => this.setState({ panel: name })}>
+        {title}
+      </li>
+    );
   }
 
   static getDerivedStateFromProps(props, state) {
     const { frame } = props;
     const panels = [];
     if (frame.text != null) {
-      panels.push("text");
-      if (!frame.hasOwnProperty("json")) {
+      panels.push('text');
+      if (!frame.hasOwnProperty('json')) {
         try {
           frame.json = JSON.parse(frame.text);
         } catch {
@@ -86,14 +165,14 @@ class FrameView extends React.Component {
         }
       }
       if (frame.json !== undefined) {
-        panels.push("json");
+        panels.push('json');
       }
     }
     if (frame.binary) {
-      panels.push("hex");
+      panels.push('hex');
     }
     if (!panels.includes(state.panel)) {
-      return {panel: panels[0]};
+      return { panel: panels[0] };
     }
     return null;
   }
@@ -104,13 +183,13 @@ class FrameView extends React.Component {
     return (
       <div className="FrameView">
         <ul className="tab-line">
-          {frame.text != null && this.makePanel("text", "Text")}
-          {frame.json !== undefined && this.makePanel("json", "JSON")}
-          {frame.binary != null && this.makePanel("hex", "Hex")}
+          {frame.text != null && this.makePanel('text', 'Text')}
+          {frame.json !== undefined && this.makePanel('json', 'JSON')}
+          {frame.binary != null && this.makePanel('hex', 'Hex')}
         </ul>
-        {panel === "text" && <TextViewer data={frame.text}/>}
-        {panel === "json" && <JsonViewer data={frame.json}/>}
-        {panel === "hex" && <HexViewer className="tab-pane" data={frame.binary}/>}
+        {panel === 'text' && <TextViewer data={frame.text} />}
+        {panel === 'json' && <JsonViewer data={frame.json} />}
+        {panel === 'hex' && <HexViewer className="tab-pane" data={frame.binary} />}
       </div>
     );
   }
@@ -120,6 +199,7 @@ export default class App extends React.Component {
   _uniqueId = 0;
   _issueTime = null;
   _issueWallTime = null;
+
   getTime(timestamp) {
     if (this._issueTime == null) {
       this._issueTime = timestamp;
@@ -128,69 +208,85 @@ export default class App extends React.Component {
     return new Date((timestamp - this._issueTime) * 1000 + this._issueWallTime);
   }
 
-  state = {frames: [], activeId: null, capturing: false};
+  state = { frames: [], activeId: null, capturing: true, regName: '' };
 
   constructor(props) {
     super(props);
 
-    props.handlers["Network.webSocketFrameReceived"] = this.webSocketFrameReceived.bind(this);
-    props.handlers["Network.webSocketFrameSent"] = this.webSocketFrameSent.bind(this);
+    props.handlers['Network.webSocketFrameReceived'] = this.webSocketFrameReceived.bind(this);
+    props.handlers['Network.webSocketFrameSent'] = this.webSocketFrameSent.bind(this);
   }
 
   render() {
-    const { frames, activeId } = this.state;
+    const { frames, activeId, regName } = this.state;
     const active = frames.find(f => f.id === activeId);
     return (
       <Panel cols className="App">
-        <FrameList size={300} minSize={180} resizable frames={frames} activeId={activeId} onClear={this.clearFrames} onSelect={this.selectFrame} onStart={this.startCapture} onStop={this.stopCapture}/>
+        <Panel size={300} minSize={180} resizable className="LeftPanel">
+          <ListControls
+            onClear={this.clearFrames}
+            onStart={this.startCapture}
+            capturing={this.state.capturing}
+            onStop={this.stopCapture}
+            regName={regName}
+            onRegName={this.setRegName}
+          />
+          <FrameList frames={frames} activeId={activeId} onSelect={this.selectFrame} regName={regName} />
+        </Panel>
         <Panel minSize={100} className="PanelView">
-          {active != null ? <FrameView frame={active}/> : <span className="message">Select a frame to view its contents</span>}
+          {active != null ? <FrameView frame={active} /> : <span className="message">Select a frame to view its contents</span>}
         </Panel>
       </Panel>
     );
   }
 
   selectFrame = id => {
-    this.setState({activeId: id});
+    this.setState({ activeId: id });
   };
 
   clearFrames = () => {
-    this.setState({frames: []});
+    this.setState({ frames: [] });
   };
 
   startCapture = () => {
-    this.setState({capturing: true});
-  }
+    this.setState({ capturing: true });
+  };
 
   stopCapture = () => {
-    this.setState({capturing: false});
-  }
+    this.setState({ capturing: false });
+  };
+
+  setRegName = e => {
+    this.setState({ regName: e.target.value });
+  };
 
   addFrame(type, timestamp, response) {
     if (response.opcode === 1 || response.opcode === 2) {
       const frame = {
         type,
+        name: type,
         id: ++this._uniqueId,
         time: this.getTime(timestamp),
-        length: response.payloadData.length,
+        length: response.payloadData.length
       };
       if (response.opcode === 1) {
         frame.text = response.payloadData;
       } else {
         frame.binary = stringToBuffer(response.payloadData);
       }
-      this.setState(({frames}) => ({frames: [...frames, frame]}));
+      this.setState(({ frames }) => ({ frames: [...frames, frame] }));
     }
   }
 
-  webSocketFrameReceived({timestamp, response}) {
+  webSocketFrameReceived({ timestamp, response }) {
     if (this.state.capturing === true) {
-      this.addFrame("incoming", timestamp, response);
+      this.addFrame('incoming', timestamp, response);
     }
   }
-  webSocketFrameSent({timestamp, response}) {
+
+  webSocketFrameSent({ timestamp, response }) {
     if (this.state.capturing === true) {
-      this.addFrame("outgoing", timestamp, response);
+      this.addFrame('outgoing', timestamp, response);
     }
   }
 }
