3e1a3d36e481aa4555fbcd71b06c24b92d87623c
nickelead <nickeleader@gmail.com>
2020-01-07 02:52:50 +0300

    refact: (ListControls.js) ListControls moved

--- src/viewer/App.js
+++ src/viewer/App.js
@@ -4,6 +4,7 @@ import FontAwesome from 'react-fontawesome';
 import cx from 'classnames';
 import HexViewer from './HexViewer';
 import {grep, TimeStamp} from './Helper.js';
+import ListControls from "./ListControls";
 
 
 import {ObjectInspector} from 'react-inspector';
@@ -17,91 +18,6 @@ const stringToBuffer = str => {
     }
     return ui8;
 };
-
-class ListControls extends React.Component {
-    state = {
-        openInput: null // 'filter' | 'name'
-    };
-
-    openNameReg = () => {
-        if (this.state.openInput === 'name') {
-            this.setState({openInput: null});
-        } else {
-            this.setState({openInput: 'name'});
-        }
-    };
-
-    openFilter = () => {
-        if (this.state.openInput === 'filter') {
-            this.setState({openInput: null});
-        } else {
-            this.setState({openInput: 'filter'});
-        }
-    };
-
-    render() {
-        const {onClear, onCapturingToggle, regName, onRegName, isCapturing, filter, onFilter, isFilterInverse, onFilterModeToggle} = this.props;
-        return (
-            <div className="list-controls">
-                <span className={isCapturing ? "list-button record active" : "list-button record"} onClick={onCapturingToggle} title={isCapturing ? "Stop" : "Start"}/>
-                <FontAwesome className="list-button" name="ban" onClick={onClear} title="Clear"/>
-                <span className={'separator'}/>
-                {/* name */}
-                <FontAwesome
-                    className={cx('list-button', {
-                        active: !!regName
-                    })}
-                    name="file-signature"
-                    onClick={this.openNameReg}
-                    title="Clear"
-                />
-                <div
-                    className={cx('input-wrap', {
-                        hide: this.state.openInput !== 'name'
-                    })}
-                >
-                    <input
-                        className={'input'}
-                        name={'reg-name'}
-                        placeholder={'Name regexp:  "type":"(\\w+)"'}
-                        value={regName}
-                        onChange={onRegName}
-                    />
-                </div>
-
-                {/* filter */}
-                <FontAwesome
-                    className={cx('list-button', {
-                        active: !!filter
-                    })}
-                    name="filter"
-                    onClick={this.openFilter}
-                    title="Filter"
-                />
-
-
-                <div
-                    className={cx('input-wrap', {
-                        hide: this.state.openInput !== 'filter'
-                    })}
-                >
-                    <input
-                        className={'input'}
-                        name={'open-filter'}
-                        placeholder={'Filter regexp'}
-                        value={filter}
-                        onChange={onFilter}
-
-                    />
-                    <FontAwesome className="list-button" onClick={onFilterModeToggle}
-                                 name={ isFilterInverse ? "check-square" : "square"}/>
-                                 invert
-
-                </div>
-            </div>
-        );
-    }
-}
 
 class FrameList extends React.Component {
     handlerClearSelect = () => {
